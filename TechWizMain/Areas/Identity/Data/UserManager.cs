d3877a562db235f2855ecce05853b686a1ea38b5
viet dong <levietdongqt@gmail.com>
2023-08-05 16:31:49 +0700

    Add project files.

---
+++ TechWizMain/Areas/Identity/Data/UserManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace TechWizMain.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the UserManager class
+public class UserManager : IdentityUser
+{
+}
+
---
+++ TechWizMain/Areas/Identity/Data/UserManagerContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TechWizMain.Areas.Identity.Data;
+
+namespace TechWizMain.Data;
+
+public class UserManagerContext : IdentityDbContext<UserManager>
+{
+    public UserManagerContext(DbContextOptions<UserManagerContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+        foreach (var entityType in builder.Model.GetEntityTypes())
+        {
+            var tableName = entityType.GetTableName(); //B2: Lay ten cua table
+            if (tableName.StartsWith("AspNet"))
+            {
+                entityType.SetTableName(tableName.Substring(6));
+            }
+        }
+    }
+}
