21df9dfbe792a981ebe7241351c2361824ff79b8
TranHuy <huy.tran9510@gmail.com>
2023-08-09 20:20:59 +0700

    mess

--- TechWizMain/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TechWizMain/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -139,6 +139,7 @@ namespace TechWizMain.Areas.Identity.Pages.Account
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
+
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
